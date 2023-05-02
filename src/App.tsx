4e3a22fd29f364b8a4fd4e38450307e0607fc9c7
BrunoLicuadri <blicuadri@gmail.com>
2023-05-02 08:39:28 -0300

    links w/out full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
-    <h1> BookKeeper </h1>
+    <div>
+      <h1> BookKeeper </h1>
+      <nav style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+      >
+        <Link to="/invoices"> Invoices</Link> | {""}
+        <Link to="/expenses"> Expenses</Link>
+      </nav>
+    </div>
+
   );
 }
 
