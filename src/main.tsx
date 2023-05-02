22ea40978bd3bed968168b46383eb22249bd1a45
BrunoLicuadri <blicuadri@gmail.com>
2023-05-02 09:28:42 -0300

    Nested Routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,10 @@ import Expenses from './routes/expenses.tsx'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/expenses" element={<Expenses/>} />
-      <Route path="/invoices" element={<Invoices/>} />
+      <Route path="/" element={<App />}>
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/invoices" element={<Invoices />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
 )
