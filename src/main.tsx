f24562fa9898553ebe60d9c0f47266733a310f28
BrunoLicuadri <blicuadri@gmail.com>
2023-05-03 08:26:29 -0300

    Index Routes for main and Invoices page

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,20 @@ import Invoices from './routes/Invoices/invoices.tsx'
 import Expenses from './routes/Expenses/expenses.tsx'
 import NotFound from './routes/NotFound/index.tsx'
 import Invoice from './routes/Invoices/invoice/index.tsx'
+import InvoiceIndex from './routes/Invoices/invoiceIndex/index.tsx'
+import Welcome from './routes/Welcome/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route index element={<Welcome/>} />
         <Route path="/expenses" element={<Expenses />} />
         <Route path="/invoices" element={<Invoices />} >
-          <Route path=":invoiceId" element={<Invoice/>} />
+          <Route index element={<InvoiceIndex />} />
+          <Route path=":invoiceId" element={<Invoice />} />
         </Route>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>,
